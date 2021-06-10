d20dd0fd9024a306ecf3ad2b85542ee8b8a36823
SebastianEchecury <sechecury@gmail.com>
2021-06-10 20:32:56 -0300

    practica

---
+++ $and, $not, $or.js
@@ -0,0 +1,33 @@
+//Metodo find. Puedo indicar todas las condiciones que quiera
+db.personas.find({"edad":15, "nombre":"Juan"})
+//Si no quiero que me traiga todos los datos del documento lo indico como un segundo parametro
+db.personas.find({"edad":15, "nombre":"Juan"},{"edad":1, "nombre":1})
+//También puedo indicar que campos no me tiene que traer poniendo el 0 en vez del 1
+db.personas.find({"edad":15, "nombre":"Juan"},{"edad":0})
+
+
+//Metodo findOne permite solo seleccionar 1 documento, a pesar de que pueda haber varios
+//Si varios documentos cumplen con la condición traera el primero que encuentra
+
+
+//Operadores condicionales
+//Traigo los documentos que la edad sea >= 18 y <=30.
+db.personas2.find({"edad":{"$gte":18,"$lte":30}})
+
+//Primero asigno a fecha la fecha de hoy, luego traigo los documentos que el registro sea < fecha.
+fecha = new Date("01/01/2015")
+db.personas2.find({"registro":{"$lt":fecha}})
+
+//Traigo los documentos que el nombre es distinto de Juan
+db.personas2.find({"nombre":{"$ne":"Juan"}})
+
+
+//Operadores logicos
+//Traigo todos los que NO son != Juan
+db.personas2.find({"nombre":{"$not":{"$ne":"Juan"}}})
+
+//Traigo los documentos cuyo nombre empieza con J y la edad es 34
+db.personas2.find({"$and":[{"nombre":/^J/},{"edad":34}]})
+
+//Traigo los documentos cuyos nombres empiezan con J o su edad es menor a 30
+db.personas2.find({"$or":[{"nombre":/^J/},{"edad":{"$lt":30}}]})
---
+++ upsert, multiples actualizaciones.js
@@ -0,0 +1,8 @@
+//El upsert es un parámetro que paso al hacer un update, lo que hace es que si no encuentra un documento que coincida con nuestra condicion, 
+//creara el documento, si lo encuentra hara la modificacion normal (debe estar en true, si no no hara nada).
+db.personas.update({"nombre":"Marco"}, {"$set":{"apellido":"Ruben"}}, true);
+
+//Para hacer multiples actualizaciones cuando hago un update hay que agregar un parametro luego del upsert, puede tomar los valores true o false. 
+//Si esta en true actualiza todos los documentos que cumplan con la condición. 
+//Dependiendo del upsert lo creara si no existe. Si estuviera en false actualiza solo 1.
+db.personas.update({"nombre":"Juan"}, {"$set":{"apellido":"Lopez"}}, true, true);
